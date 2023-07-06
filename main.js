c1ad57db59c98dc8b45181b874bfcf35ff6152d4
m <ehdeha@gmail.com>
2023-07-06 18:58:23 +0900

    0706

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ promotionToggleBtn.addEventListener('click', function() {
 });
 
 // 클릭하면 숨긴다 = 원래는 나타나있다.
+
+
+const headerEl = document.querySelector('.logo-box')
+const bannerEl = document.querySelector('.banner')
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 700) {
+        headerEl.classList.add('scroll')
+        bannerEl.classList.add('scroll')
+    }else{
+        headerEl.classList.remove('scroll')
+        bannerEl.classList.remove('scroll')
+    }
+})
