97ad3f9fa3cc61a1758f18090b64f20218c490c7
m <ehdeha@gmail.com>
2023-07-11 23:41:59 +0900

    07.11

--- main.js
+++ main.js
@@ -13,8 +13,6 @@ promotionToggleBtn.addEventListener('click', function() {
 });
 
 // 클릭하면 숨긴다 = 원래는 나타나있다.
-
-
 const headerEl = document.querySelector('.logo-box')
 const bannerEl = document.querySelector('.banner')
 window.addEventListener('scroll', function(){
@@ -29,15 +27,60 @@ window.addEventListener('scroll', function(){
 
 
 
+const slides = document.querySelectorAll('.cody .cody__container');
+let currentSlideIndex = 0;
+let timer = null;
 
+function showSlide(index) {
+  slides.forEach(slide => {
+    slide.style.transform = `translateX(-${index * 100}vw)`;
+  });
+}
 
+function prevSlide() {
+  currentSlideIndex--;
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+  }
+  showSlide(currentSlideIndex);
+}
 
-const allEl = document.querySelector('intultive-btn button:first')
+function nextSlide() {
+  currentSlideIndex++;
+  if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+  }
+  showSlide(currentSlideIndex);
+}
 
+function startSlideShow() {
+  timer = setInterval(() => {
+    nextSlide();
+  }, 3500);
+}
 
+function stopSlideShow() {
+  clearInterval(timer);
+}
 
+// 이전 버튼 클릭 시 이벤트 핸들러
+document.querySelectorAll('.prev').forEach(button => {
+  button.addEventListener('click', () => {
+    stopSlideShow();
+    prevSlide();
+    startSlideShow();
+  });
+});
 
-//  수정해야할 부분들
-//  banner 부분 스크롤시 화면 최상단으로 올라가지 않음
-//  이미지 슬라이드 구현
- 
+// 다음 버튼 클릭 시 이벤트 핸들러
+document.querySelectorAll('.next').forEach(button => {
+  button.addEventListener('click', () => {
+    stopSlideShow();
+    nextSlide();
+    startSlideShow();
+  });
+});
+
+// 초기 슬라이드 표시 및 자동 슬라이드 시작
+showSlide(currentSlideIndex);
+startSlideShow();
